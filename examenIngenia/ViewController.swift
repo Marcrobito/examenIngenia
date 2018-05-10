5b22a8daa04e19f831e8bbdf5409519d50a61c4f
Marcrobito <marco.ant.mtz@gmail.com>
2018-05-10 09:29:17 -0500

    Initial Commit

---
+++ examenIngenia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  examenIngenia
+//
+//  Created by Marco Antonio Martínez Gutiérrez on 10/05/18.
+//  Copyright © 2018 Marco Antonio Martínez Gutiérrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
